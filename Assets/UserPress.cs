f28703f9b65ed5acd921aac68edfb2880c4d5627
Unknown <deovolentegames@gmail.com>
2020-02-02 11:14:17 +1300

    New land rover and Nodey changes

--- Assets/UserPress.cs
+++ Assets/UserPress.cs
@@ -6,8 +6,21 @@ using Doozy.Engine;
 [RequireComponent(typeof(MeshCollider))]
 public class UserPress : MonoBehaviour
 {
+    public int ClicksToRemove;
+
     void OnMouseDown()
     {
         GameEventMessage.SendEvent("Sabotage", gameObject);
+    }
+
+    void DealDamage()
+    {
+        ClicksToRemove--;
+        if ()
+    }
+
+    bool IsAlive()
+    {
+        return ClicksToRemove > 0;
     }
 }
