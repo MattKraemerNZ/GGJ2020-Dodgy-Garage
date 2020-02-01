09238a6002cf06902f46ff048f01235abbca60d9
Matt <matt@kraemer.co.nz>
2020-02-02 09:56:40 +1300

    Initial Commit

---
+++ Assets/UserPress.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine;
+
+[RequireComponent(typeof(MeshCollider))]
+public class UserPress : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GameEventMessage.SendEvent("Sabotage", gameObject);
+    }
+}
